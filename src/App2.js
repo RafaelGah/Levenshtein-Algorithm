3b723475fcafa7d75dd8d5cc56c5fb69933301b8
RafaelGahler <rafael.gahler@stud.kbw.ch>
2022-03-09 12:25:32 +0100

    App2 fix

--- src/App2.js
+++ src/App2.js
@@ -1,32 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import App from "./App";
 
-function App() {
+function App2() {
 
     const [input, setInput] = useState("")
     const [ausgabe, setAusgabe] = useState(0)
     const [input2, setInput2] = useState("")
 
-    const levenshteinDistance = (str1 = '', str2 = '') => {
-        const track = Array(str2.length + 1).fill(null).map(() => Array(str1.length + 1).fill(null));
-        for (let i = 0; i <= str1.length; i += 1) {
-            track[0][i] = i;
-        }
-        for (let j = 0; j <= str2.length; j += 1) {
-            track[j][0] = j;
-        }
-        for (let j = 1; j <= str2.length; j += 1) {
-            for (let i = 1; i <= str1.length; i += 1) {
-                const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
-                track[j][i] = Math.min(track[j][i - 1] + 1, // deletion
-                    track[j - 1][i] + 1, //insertion
-                    track[j - 1][i - 1] + indicator, // substitution
-                );
-            }
-        }
-        return track[str2.length][str1.length];
-    }
+
 
     function buttonHandler() {
         let result = levenshteinDistance(input, input2)
@@ -62,3 +45,23 @@ function App() {
     );
 
 }
+const levenshteinDistance = (str1 = '', str2 = '') => {
+    const track = Array(str2.length + 1).fill(null).map(() => Array(str1.length + 1).fill(null));
+    for (let i = 0; i <= str1.length; i += 1) {
+        track[0][i] = i;
+    }
+    for (let j = 0; j <= str2.length; j += 1) {
+        track[j][0] = j;
+    }
+    for (let j = 1; j <= str2.length; j += 1) {
+        for (let i = 1; i <= str1.length; i += 1) {
+            const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+            track[j][i] = Math.min(track[j][i - 1] + 1, // deletion
+                track[j - 1][i] + 1, //insertion
+                track[j - 1][i - 1] + indicator, // substitution
+            );
+        }
+    }
+    return track[str2.length][str1.length];
+}
+export default App2;
